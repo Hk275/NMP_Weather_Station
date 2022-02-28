9e7de0b5115ab108c4a9f55dff53a5e36977ced7
Soura Mandal <mandal.soura@gmail.com>
2022-02-28 11:57:40 +1100

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -52,9 +52,9 @@ async function sendData(): Promise<void> {
 
 socket.bind(4500);
 
-setInterval(sendData, 1000);
-
 // -------------------------------------------------------
+
+setInterval(sendData, 1000);
 
 api.listen(API_PORT, () =>
   console.log(
